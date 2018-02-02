a066846d6eaed0733b70dca8ab852c49744b4923
Devon Ravihansa <devonravihansawiz@gmail.com>
2018-02-02 20:45:32 +0530

    Create sidebar.js

---
+++ sidebar/sidebar.js
@@ -0,0 +1,8 @@
+ $("#menu-toggle-left").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled-left");
+    });
+     $("#menu-toggle-right").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled-right");
+    });
