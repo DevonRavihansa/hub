b4c6a70a81d2cb1b4ad31751f7376c48f763757d
Devon Ravihansa <devonravihansawiz@gmail.com>
2018-02-02 21:36:24 +0530

    Update sidebar.js

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,8 +1,8 @@
- $("#menu-toggle-left").click(function(e) {
+$("#toggle-left").click(function(e) {
         e.preventDefault();
-        $("#wrapper").toggleClass("toggled-left");
-    });
-     $("#menu-toggle-right").click(function(e) {
+        $(".wrapper").toggleClass("toggled-left");
+});
+$("#toggle-right").click(function(e) {
         e.preventDefault();
-        $("#wrapper").toggleClass("toggled-right");
-    });
+        $(".wrapper").toggleClass("toggled-right");
+});
